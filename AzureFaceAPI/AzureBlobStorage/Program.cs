99c10bac237b93bd44d353bcdef9c66e07335b5c
Wayne <jhywen022@gmail.com>
2021-12-09 13:02:08 +0800

    Azure Initial

---
+++ AzureFaceAPI/AzureBlobStorage/Program.cs
@@ -0,0 +1,145 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorage
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Blob Storage v12 - .NET quickstart sample\n");
+
+            // Blob Storage 連線字串
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=waynemachinele2852186040;AccountKey=KDWrY7BuJteNlWrhrJq/g7QbkVg8YUGBWoL2SFI2X4xV3UH9DpAJZYQPYiGUMweuyPVpoAEd3JxwYBruLZrU4A==;EndpointSuffix=core.windows.net";
+
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            Console.WriteLine("----------Container List----------");
+            Console.WriteLine();
+            GetBlobContainers(blobServiceClient);
+            Console.WriteLine();
+
+            Console.WriteLine("----------CreateSampleContainerAsync----------");
+            Console.WriteLine();
+            await CreateSampleContainerAsync(blobServiceClient, "test");
+            Console.WriteLine();
+
+            Console.WriteLine("----------Container List----------");
+            Console.WriteLine();
+            GetBlobContainers(blobServiceClient);
+            Console.WriteLine();
+
+            Console.WriteLine("----------DeleteContainersWithPrefixAsync----------");
+            await DeleteContainersWithPrefixAsync(blobServiceClient, "test");
+            Console.WriteLine();
+
+            Console.WriteLine("----------Container List----------");
+            Console.WriteLine();
+            GetBlobContainers(blobServiceClient);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        //-------------------------------------------------
+        // Get container list
+        //-------------------------------------------------
+        public static void GetBlobContainers(BlobServiceClient blobServiceClient)
+        {
+            // 列出Blob Storage所有Container List
+            var containerList = blobServiceClient.GetBlobContainers();
+
+            foreach (BlobContainerItem blobcontainer in containerList)
+            {
+                Console.WriteLine(blobcontainer.Name);
+            }
+        }
+
+        //-------------------------------------------------
+        // Create a container
+        //-------------------------------------------------
+        private static async Task<BlobContainerClient> CreateSampleContainerAsync(BlobServiceClient blobServiceClient, string containerName)
+        {
+            // Name the sample container based on new GUID to ensure uniqueness.
+            // The container name must be lowercase.
+            containerName += Guid.NewGuid();
+
+            try
+            {
+                // Create the container
+                BlobContainerClient container = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+                if (await container.ExistsAsync())
+                {
+                    Console.WriteLine("Created container {0}", container.Name);
+                    return container;
+                }
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine("HTTP error code {0}: {1}",
+                                    e.Status, e.ErrorCode);
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+
+
+        //-------------------------------------------------
+        // Delete a container
+        //-------------------------------------------------
+        private static async Task DeleteSampleContainerAsync(BlobServiceClient blobServiceClient, string containerName)
+        {
+            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName);
+
+            try
+            {
+                // Delete the specified container and handle the exception.
+                await container.DeleteAsync();
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine("HTTP error code {0}: {1}",
+                                    e.Status, e.ErrorCode);
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+
+
+        //-------------------------------------------------
+        // Delete all containers with the specified prefix
+        //-------------------------------------------------
+        private static async Task DeleteContainersWithPrefixAsync(BlobServiceClient blobServiceClient, string prefix)
+        {
+            Console.WriteLine($"Delete all containers beginning with the specified prefix {prefix}");
+
+            try
+            {
+                foreach (BlobContainerItem container in blobServiceClient.GetBlobContainers())
+                {
+                    if (container.Name.StartsWith(prefix))
+                    {
+                        Console.WriteLine("\tContainer:" + container.Name);
+                        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(container.Name);
+                        await containerClient.DeleteAsync();
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+    }
+}
---
+++ AzureFaceAPI/AzureFaceAPI/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace AzureFaceAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // From your Face subscription in the Azure portal, get your subscription key and endpoint.
+            const string SUBSCRIPTION_KEY = "540b1bef82044009889e7107fce1282f";
+            const string ENDPOINT = "https://waynefaceapi.cognitiveservices.azure.com/";
+
+            // Used for all examples.
+            // URL for the images.
+            //const string IMAGE_BASE_URL = "https://csdx.blob.core.windows.net/resources/Face/Images/";
+            const string IMAGE_BASE_URL = "        https://waynemachinele2852186040.blob.core.windows.net/faceapicontainer/";
+
+            // Recognition model 4 was released in 2021 February.
+            // It is recommended since its accuracy is improved
+            // on faces wearing masks compared with model 3,
+            // and its overall accuracy is improved compared
+            // with models 1 and 2.
+            const string RECOGNITION_MODEL4 = RecognitionModel.Recognition04;
+
+            // Authenticate.
+            IFaceClient client = Authenticate(ENDPOINT, SUBSCRIPTION_KEY);
+
+            // Detect - get features from faces.
+            await DetectFaceExtract(client, IMAGE_BASE_URL, RECOGNITION_MODEL4);
+            // Find Similar - find a similar face from a list of faces.
+
+            //await FindSimilar(client, IMAGE_BASE_URL, RECOGNITION_MODEL4);
+            //// Verify - compare two images if the same person or not.
+            ////Verify(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+
+            //// Identify - recognize a face(s) in a person group (a person group is created in this example).
+            //IdentifyInPersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            //// LargePersonGroup - create, then get data.
+            //LargePersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            //// Group faces - automatically group similar faces.
+            //Group(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            //// FaceList - create a face list, then get data
+
+
+
+            Console.ReadLine();
+        }
+
+        /*
+         *	AUTHENTICATE
+         *	Uses subscription key and region to create a client.
+         */
+        public static IFaceClient Authenticate(string endpoint, string key)
+        {
+            return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+
+
+        /* 
+         * DETECT FACES
+         * Detects features from faces and IDs them.
+         */
+        public static async Task DetectFaceExtract(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========DETECT FACES========");
+            Console.WriteLine();
+
+            // Create a list of images
+            List<string> imageFileNames = new List<string>
+                    {
+                        "detection1.jpg",    // single female with glasses
+                        "detection2.jpg", // (optional: single man)
+                        "detection3.jpg", // (optional: single male construction worker)
+                        "detection4.jpg", // (optional: 3 people at cafe, 1 is blurred)
+                        "detection5.jpg",    // family, woman child man
+                        "detection6.jpg"     // elderly couple, male female
+                    };
+
+            foreach (var imageFileName in imageFileNames)
+            {
+                IList<DetectedFace> detectedFaces;
+                //
+
+                detectedFaces = await client.Face.DetectWithUrlAsync($"{url}{imageFileName}",
+                returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+                                FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+                                FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+                                FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile },
+
+                // We specify detection model 1 because we are retrieving attributes.
+                detectionModel: DetectionModel.Detection01,
+                recognitionModel: recognitionModel);
+
+                Console.WriteLine($"{detectedFaces.Count} face(s) detected from image `{imageFileName}`.");
+
+
+
+                // Parse and print all attributes of each detected face.
+                foreach (var face in detectedFaces)
+                {
+                    Console.WriteLine($"Face attributes for {imageFileName}:");
+
+                    // Get bounding box of the faces
+                    Console.WriteLine($"Rectangle(Left/Top/Width/Height) : {face.FaceRectangle.Left} {face.FaceRectangle.Top} {face.FaceRectangle.Width} {face.FaceRectangle.Height}");
+
+                    // Get accessories of the faces
+                    List<Accessory> accessoriesList = (List<Accessory>)face.FaceAttributes.Accessories;
+                    int count = face.FaceAttributes.Accessories.Count;
+                    string accessory; string[] accessoryArray = new string[count];
+                    if (count == 0) { accessory = "NoAccessories"; }
+                    else
+                    {
+                        for (int i = 0; i < count; ++i) { accessoryArray[i] = accessoriesList[i].Type.ToString(); }
+                        accessory = string.Join(",", accessoryArray);
+                    }
+                    Console.WriteLine($"Accessories : {accessory}");
+
+                    // Get face other attributes
+                    Console.WriteLine($"Age : {face.FaceAttributes.Age}");
+                    Console.WriteLine($"Blur : {face.FaceAttributes.Blur.BlurLevel}");
+
+                    // Get emotion on the face
+                    string emotionType = string.Empty;
+                    double emotionValue = 0.0;
+                    Emotion emotion = face.FaceAttributes.Emotion;
+                    if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
+                    if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
+                    if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
+                    if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
+                    if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
+                    if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
+                    if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
+                    if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
+                    Console.WriteLine($"Emotion : {emotionType}");
+
+                    // Get more face attributes
+                    Console.WriteLine($"Exposure : {face.FaceAttributes.Exposure.ExposureLevel}");
+                    Console.WriteLine($"FacialHair : {string.Format("{0}", face.FaceAttributes.FacialHair.Moustache + face.FaceAttributes.FacialHair.Beard + face.FaceAttributes.FacialHair.Sideburns > 0 ? "Yes" : "No")}");
+                    Console.WriteLine($"Gender : {face.FaceAttributes.Gender}");
+                    Console.WriteLine($"Glasses : {face.FaceAttributes.Glasses}");
+
+                    // Get hair color
+                    Hair hair = face.FaceAttributes.Hair;
+                    string color = null;
+                    if (hair.HairColor.Count == 0) { if (hair.Invisible) { color = "Invisible"; } else { color = "Bald"; } }
+                    HairColorType returnColor = HairColorType.Unknown;
+                    double maxConfidence = 0.0f;
+                    foreach (HairColor hairColor in hair.HairColor)
+                    {
+                        if (hairColor.Confidence <= maxConfidence) { continue; }
+                        maxConfidence = hairColor.Confidence; returnColor = hairColor.Color; color = returnColor.ToString();
+                    }
+                    Console.WriteLine($"Hair : {color}");
+
+                    // Get more attributes
+                    Console.WriteLine($"HeadPose : {string.Format("Pitch: {0}, Roll: {1}, Yaw: {2}", Math.Round(face.FaceAttributes.HeadPose.Pitch, 2), Math.Round(face.FaceAttributes.HeadPose.Roll, 2), Math.Round(face.FaceAttributes.HeadPose.Yaw, 2))}");
+                    Console.WriteLine($"Makeup : {string.Format("{0}", (face.FaceAttributes.Makeup.EyeMakeup || face.FaceAttributes.Makeup.LipMakeup) ? "Yes" : "No")}");
+                    Console.WriteLine($"Noise : {face.FaceAttributes.Noise.NoiseLevel}");
+                    Console.WriteLine($"Occlusion : {string.Format("EyeOccluded: {0}", face.FaceAttributes.Occlusion.EyeOccluded ? "Yes" : "No")} " +
+                        $" {string.Format("ForeheadOccluded: {0}", face.FaceAttributes.Occlusion.ForeheadOccluded ? "Yes" : "No")}   {string.Format("MouthOccluded: {0}", face.FaceAttributes.Occlusion.MouthOccluded ? "Yes" : "No")}");
+                    Console.WriteLine($"Smile : {face.FaceAttributes.Smile}");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        /*
+         * FIND SIMILAR
+         * This example will take an image and find a similar one to it in another image.
+         */
+        public static async Task FindSimilar(IFaceClient client, string url, string recognition_model)
+        {
+            Console.WriteLine("========FIND SIMILAR========");
+            Console.WriteLine();
+
+            List<string> targetImageFileNames = new List<string>
+                        {
+                            "Family1-Dad1.jpg",
+                            "Family1-Daughter1.jpg",
+                            "Family1-Mom1.jpg",
+                            "Family1-Son1.jpg",
+                            "Family2-Lady1.jpg",
+                            "Family2-Man1.jpg",
+                            "Family3-Lady1.jpg",
+                            "Family3-Man1.jpg"
+                        };
+
+            string sourceImageFileName = "findsimilar.jpg";
+            IList<Guid?> targetFaceIds = new List<Guid?>();
+            foreach (var targetImageFileName in targetImageFileNames)
+            {
+                // Detect faces from target image url.
+                var faces = await DetectFaceRecognize(client, $"{url}{targetImageFileName}", recognition_model);
+                // Add detected faceId to list of GUIDs.
+                targetFaceIds.Add(faces[0].FaceId.Value);
+            }
+
+            // Detect faces from source image url.
+            IList<DetectedFace> detectedFaces = await DetectFaceRecognize(client, $"{url}{sourceImageFileName}", recognition_model);
+            Console.WriteLine();
+
+            // Find a similar face(s) in the list of IDs. Comapring only the first in list for testing purposes.
+            IList<SimilarFace> similarResults = await client.Face.FindSimilarAsync(detectedFaces[0].FaceId.Value, null, null, targetFaceIds);                       
+        }
+
+        
+
+        public static async Task IdentifyInPersonGroup(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========IDENTIFY FACES========");
+            Console.WriteLine();
+
+            // Create a dictionary for all your images, grouping similar ones under the same key.
+            Dictionary<string, string[]> personDictionary =
+                new Dictionary<string, string[]>
+                    { { "Family1-Dad", new[] { "Family1-Dad1.jpg", "Family1-Dad2.jpg" } },
+                      { "Family1-Mom", new[] { "Family1-Mom1.jpg", "Family1-Mom2.jpg" } },
+                      { "Family1-Son", new[] { "Family1-Son1.jpg", "Family1-Son2.jpg" } },
+                      { "Family1-Daughter", new[] { "Family1-Daughter1.jpg", "Family1-Daughter2.jpg" } },
+                      { "Family2-Lady", new[] { "Family2-Lady1.jpg", "Family2-Lady2.jpg" } },
+                      { "Family2-Man", new[] { "Family2-Man1.jpg", "Family2-Man2.jpg" } }
+                    };
+            // A group photo that includes some of the persons you seek to identify from your dictionary.
+            string sourceImageFileName = "identification1.jpg";
+        }
+
+        private static async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient faceClient, string url, string recognition_model)
+        {
+            // Detect faces from image URL. Since only recognizing, use the recognition model 1.
+            // We use detection model 3 because we are not retrieving attributes.
+            IList<DetectedFace> detectedFaces = await faceClient.Face.DetectWithUrlAsync(url, recognitionModel: recognition_model, detectionModel: DetectionModel.Detection03);
+            Console.WriteLine($"{detectedFaces.Count} face(s) detected from image `{Path.GetFileName(url)}`");
+            return detectedFaces.ToList();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
